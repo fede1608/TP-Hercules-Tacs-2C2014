83021bb243aff3e9ba08e6c74c0031bf8702739a
UrielMys <uri_mysler@hotmail.com>
2014-09-01 16:43:46 -0300

    items de mercado libre para traer

--- truequelibre/truequelibre-war/src/main/java/com/hercules/truequelibre/ItemsResource.java
+++ truequelibre/truequelibre-war/src/main/java/com/hercules/truequelibre/ItemsResource.java
@@ -17,7 +17,12 @@ import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.Parameter;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.hercules.truequelibre.FacebookDataCollector;
+import com.hercules.truequelibre.mlsdk.Meli;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import javax.ws.rs.core.MultivaluedMap;
 
 
 public class ItemsResource extends ServerResource {
@@ -33,26 +38,53 @@ public class ItemsResource extends ServerResource {
 
 	@Override
 	protected Representation get() throws ResourceException {
+		Meli m = new Meli(7937694478293453L, "UUY3czo96JZDtnsFI2iMt0vIzMBukOtB");
 		String message = "Trueque Libre!"
 			+ "\n la pagina que ingreso es: " + this.getReference()
 			+ "\n con el recurso: " + this.getReference().getBaseRef()
 			+ "\n con el numero de usuario: " + (String) this.getRequest().getAttributes().get("userId")
-			+ "\n con el item pedido" + this.requestedItem()
+			+ "\n con el item pedido: " + this.requestedItem()
 			+ "\n";
+		
 		Series<Cookie> cookies = getCookies();	
 		String token = cookies.getValues("accessToken");
 		if (FacebookDataCollector.getInstance().informationCanBeShown(token, this.requestedUser())){
 			message += "te puedo mostrar la info del item que es: " + this.requestedItem();
 			if (this.itemExists()){
-				message+= "el item pedido existe entre los suyos!";
+				message+= "\n el item pedido existe entre los suyos!";
+				message+=this.itemInfo(m,this.requestedItem());
 			}else{
-				message += "no tiene el item entre sus items";
+				message += "\n no tiene el item entre sus items";
 			}
 		}else{
 			message += "la persona no es amigo suyo";
 		}
 		
 		return new StringRepresentation(message, MediaType.TEXT_PLAIN);
+	}
+
+	public String itemInfo(Meli m,String requestedItem) {
+		JsonArray search= new JsonArray();
+		JsonObject algo;
+		try {
+	       JsonObject response= m.get("/items/"+this.requestedItem());
+	       JsonArray results=response.getAsJsonArray("results");
+	       algo=response;
+	       for(int i=0; i<results.size();i++){
+	    	   JsonObject item= results.get(i).getAsJsonObject();
+	    	   JsonObject searchItem= new JsonObject();
+	    	   searchItem.add("id", item.get("id"));
+	    	   searchItem.add("img", item.get("thumbnail"));
+	    	   searchItem.add("name", item.get("title"));
+	    	   search.add(searchItem);
+	       }
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return algo.toString();
+		//return search.toString();
 	}
 
 	private String requestedUser(){
