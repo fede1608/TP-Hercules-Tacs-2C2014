615c32f01cf1ab75d28643275ff2c1df50a968ad
Juan Jacobs <juan.marcos.jacobs@grada.com.ar>
2014-09-04 12:16:49 -0300

    Agregando ExchangeTL

---
+++ truequelibre/truequelibre-war/src/main/java/com/hercules/truequelibre/ExchangeTL.java
@@ -0,0 +1,66 @@
+package com.hercules.truequelibre;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
+
+public class ExchangeTL {
+	@Id public String id;
+	public UserTL requesterUser;
+	public UserTL requestedUser;
+	public ItemTL offeredItem;
+	public ItemTL wantedItem;
+	
+	
+	public void acceptExchange() {
+		this.getRequesterUser().items.remove(offeredItem);
+		this.getRequestedUser().items.add(offeredItem);
+		this.getRequesterUser().items.add(wantedItem);
+		this.getRequestedUser().items.remove(wantedItem);
+	}
+
+	public ExchangeTL(UserTL requesterUser, UserTL requestedUser,
+			ItemTL offeredItem, ItemTL wantedItem) {
+		
+		this.requesterUser = requesterUser;
+		this.requestedUser = requestedUser;
+		this.offeredItem = offeredItem;
+		this.wantedItem = wantedItem;
+	}
+
+	public UserTL getRequesterUser() {
+		return requesterUser;
+	}
+
+	public void setRequesterUser(UserTL requesterUser) {
+		this.requesterUser = requesterUser;
+	}
+
+	public UserTL getRequestedUser() {
+		return requestedUser;
+	}
+
+	public void setRequestedUser(UserTL requestedUser) {
+		this.requestedUser = requestedUser;
+	}
+
+	public ItemTL getOfferedItem() {
+		return offeredItem;
+	}
+
+	public void setOfferedItem(ItemTL offeredItem) {
+		this.offeredItem = offeredItem;
+	}
+
+	public ItemTL getWantedItem() {
+		return wantedItem;
+	}
+
+	public void setWantedItem(ItemTL wantedItem) {
+		this.wantedItem = wantedItem;
+	}
+
+
+
+}
