ebd5c8988ff384c4ae85590a19200a7949530391
Jonathan Coralo <jonathan.corallo92@gmail.com>
2014-09-04 21:22:38 -0300

    Prueba persistencia 1:M y gitignore

--- truequelibre/truequelibre-war/src/main/java/com/hercules/truequelibre/SearchResource.java
+++ truequelibre/truequelibre-war/src/main/java/com/hercules/truequelibre/SearchResource.java
@@ -3,6 +3,10 @@ package com.hercules.truequelibre;
 //Se importa estaticamente para poder utilizar los metodos definidos en ofy sin instanciarlo
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
+import java.util.List;
+
+import com.googlecode.objectify.cmd.Query;
+
 import org.restlet.Context;
 import org.restlet.Request;
 import org.restlet.Response;
@@ -19,6 +23,7 @@ import com.google.gson.JsonObject;
 import com.googlecode.objectify.Key;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.Result;
 import com.hercules.truequelibre.mlsdk.Meli;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
@@ -56,6 +61,8 @@ public class SearchResource extends ServerResource {
 		JsonArray search= new JsonArray();
 		String resultadoPersistido ="\n";
 		ItemTL itemPersistido = null;
+		ItemTL itemSolicitud = null;
+		ItemTL itemSolicitud2 = null;
 		try {
 	       JsonObject response= m.get("sites/MLA/search", params);
 	       JsonArray results=response.getAsJsonArray("results");
@@ -67,7 +74,15 @@ public class SearchResource extends ServerResource {
 	    	   searchItem.add("img", item.get("thumbnail"));
 	    	   searchItem.add("name", item.get("title"));
 	    	   //se crea el item a persistir y se almacena la entidad en la base de datos
-	    	   itemPersistido = new ItemTL(item.get("id").toString(),item.get("title").toString());
+	    	   itemPersistido = new ItemTL(item.get("title").toString());
+	    	   itemSolicitud = new ItemTL("unItemEjemploDeSolicitud");
+	    	   itemSolicitud2 = new ItemTL("unItemEjemploDeSolicitud2");
+	    	   ofy().save().entity(itemSolicitud).now(); //cuando persisto el objeto me autogenera un valor donde puse @Id
+	    	   ofy().save().entity(itemSolicitud2).now();
+	    	   Ref<ItemTL> ref1 = Ref.create(Key.create(ItemTL.class,itemSolicitud.id)); //armo unas Refs para que se pueda aplicar get() directamente
+	    	   Ref<ItemTL> ref2 = Ref.create(Key.create(ItemTL.class,itemSolicitud2.id));
+	    	   itemPersistido.agregarSolicitud(ref1);
+	    	   itemPersistido.agregarSolicitud(ref2);
 	    	   ofy().save().entity(itemPersistido).now();
 	    	   search.add(searchItem);
 	       }
@@ -76,10 +91,22 @@ public class SearchResource extends ServerResource {
 			e.printStackTrace();
 		}
 		Key<ItemTL> clave = Key.create(ItemTL.class, itemPersistido.id); //se crea una clave para acceder al item
-		Result<ItemTL> result = ofy().load().key(clave); //la clave era del ultimo item agregado y la carga es asincronica
-		ItemTL fetched1 = result.now(); //cuando le mando now() recien ahi instancia el objeto
+		ItemTL fetched1 = ofy().load().key(clave).safe(); //la clave era del ultimo item agregado, safe() me
 		resultadoPersistido += fetched1.id+"\n";
 		resultadoPersistido += fetched1.nombre+"\n";
+		resultadoPersistido += "Lista de solicitudes:\n";
+		resultadoPersistido += "Primer item de las solicitudes:\n";
+		if(fetched1.solicitudesDeIntercambio.isEmpty()){
+			resultadoPersistido += "solicitudesDeIntercambio esta vacio";
+		}
+		else{
+			List<ItemTL> listaSolicitudes =  fetched1.getSolicitudes();
+			ItemTL itemRecuperado = listaSolicitudes.get(0);
+			resultadoPersistido	+= itemRecuperado.nombre + "\n";
+			resultadoPersistido += "Segundo item de las solicitudes:\n";
+			itemRecuperado = listaSolicitudes.get(1);
+			resultadoPersistido	+= itemRecuperado.nombre + "\n";
+		}
 		return new StringRepresentation(search.toString()+resultadoPersistido, MediaType.TEXT_PLAIN);
 	}
 
