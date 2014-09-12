2b57ef414913ac54b8e0d1dbf43837aa8b0b4c98
UrielMys <uri_mysler@hotmail.com>
2014-09-12 00:49:47 -0300

    no se bien como describir esto, es la excepcion para cuando el user y el dueño del item no coinciden

---
+++ truequelibre/truequelibre-war/src/main/java/com/hercules/truequelibre/domain/UsersDontMatchException.java
@@ -0,0 +1,23 @@
+package com.hercules.truequelibre.domain;
+
+
+@SuppressWarnings("serial")
+public class UsersDontMatchException extends Exception {
+	
+	
+	private String requestedId;
+	private String requestedItemId;
+	
+	public UsersDontMatchException(String requested,String requestedItem){
+		super();
+		
+		requestedId=requested;
+		requestedItemId=requestedItem;
+	}
+
+
+	
+	public String getMessage(){
+		return "el usuario pedido: "+ requestedId + " no es el dueño del item pedido: "+requestedItemId;
+	}
+}
