aa0d1f68438874420ea854a48dd3aac7ff1dd4ab
fede1608 <fedecasares1993@yahoo.com.ar>
2014-09-27 17:07:40 -0300

    Fix fechas en index e historial

--- truequelibre/truequelibre-war/src/main/webapp/js/truequelibre/index.js
+++ truequelibre/truequelibre-war/src/main/webapp/js/truequelibre/index.js
@@ -9,12 +9,19 @@ app.config( [
 
 app.filter("sanitize", ['$sce', function($sce) {
   return function(htmlCode){
-            return $sce.trustAsHtml(htmlCode);
-            }
-        }
-    ]);
+    return $sce.trustAsHtml(htmlCode);
+}
+}
+]);
 
 function feedController($scope) {
+    $scope.formatDate=function(UNIX_timestamp){//TODO buscar forma de llamar a una funcion externa
+        //getFormattedDate(data);
+        var date = new Date(UNIX_timestamp*1000);
+        var months = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
+        return date.getDate()+' de '+months[date.getMonth()] +' de '+date.getFullYear()  +' a las '+(date.getHours()<10?('0'+date.getHours()):date.getHours()) + ':' + ((date.getMinutes()<10)?('0'+date.getMinutes()):date.getMinutes());
+
+    };
     $scope.getOwnerName = function(item){
         var name;
         if(item.owner == getCookie("userID")){
@@ -28,7 +35,6 @@ function feedController($scope) {
     $('#cargandoModal').modal('show');
     $.getJSON('/api/feed', function (data) {
         console.log(data);
-        var i = 0;
         var w = 0;
         data.items.forEach(
             function(item){
--- truequelibre/truequelibre-war/src/main/webapp/js/truequelibre/truequelibre.js
+++ truequelibre/truequelibre-war/src/main/webapp/js/truequelibre/truequelibre.js
@@ -16,12 +16,12 @@ if(getCookie("accessToken")==null){
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-	    deleteCookie("accessToken");
-		deleteCookie("userID");
-		setCookie("accessToken",response.authResponse.accessToken);
-		setCookie("userID",response.authResponse.userID);
-		userId=response.authResponse.userID;
-		token=response.authResponse.accessToken;
+      deleteCookie("accessToken");
+      deleteCookie("userID");
+      setCookie("accessToken",response.authResponse.accessToken);
+      setCookie("userID",response.authResponse.userID);
+      userId=response.authResponse.userID;
+      token=response.authResponse.accessToken;
       initializeBar();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
@@ -32,7 +32,7 @@ if(getCookie("accessToken")==null){
       window.location = "login.html";
     }
 	fbApiInit = true; //init flag
-  }
+}
 
   // This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
@@ -46,15 +46,15 @@ if(getCookie("accessToken")==null){
   }
 
   window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '595790490538541',
+    FB.init({
+      appId      : '595790490538541',
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
-	status	   : true,	
+                        status	   : true,	
     xfbml      : true,  // parse social plugins on this page
     version    : 'v2.1' // use version 2.1
   });
-	
+    
   // Now that we've initialized the JavaScript SDK, we call 
   // FB.getLoginStatus().  This function gets the state of the
   // person visiting this page and can return one of three states to
@@ -70,8 +70,8 @@ if(getCookie("accessToken")==null){
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
   });
-	
-  };
+  
+};
 
   // Load the SDK asynchronously
   (function(d, s, id) {
@@ -87,189 +87,188 @@ if(getCookie("accessToken")==null){
   function initializeBar() {
     console.log('Welcome!  Fetching your information.... ');
     //debugger;
-	
+    
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
       console.log(response);
       //debugger;
       document.getElementById('profile_name').innerHTML = response.name;
     });
-	FB.api("/me/picture?width=18&height=20",  function(response) {
+    FB.api("/me/picture?width=18&height=20",  function(response) {
 
-        document.getElementById('profile_photo').setAttribute('src', response.data.url);
+      document.getElementById('profile_photo').setAttribute('src', response.data.url);
 
-	});
-	loadNotifications();
+    });
+    loadNotifications();
   }
   
   function setCookie(cname, cvalue) {
-	  document.cookie = cname + "=" + cvalue;
-	}
-  function getCookie(cname) {
-	    var name = cname + "=";
-	    var ca = document.cookie.split(';');
-	    for(var i=0; i<ca.length; i++) {
-	        var c = ca[i];
-	        while (c.charAt(0)==' ') c = c.substring(1);
-	        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
-	    }
-	    return "";
-	}
-  function deleteCookie(name) {
-	    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-	};
-  function logout() { 
-  	FB.api('/'+userId+'/permissions', 'delete',{ access_token : token } ,function(){ });
-	window.location= "login.html";
-  }
-function getUrlParameter(sParam)
-{
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
+   document.cookie = cname + "=" + cvalue;
+ }
+ function getCookie(cname) {
+   var name = cname + "=";
+   var ca = document.cookie.split(';');
+   for(var i=0; i<ca.length; i++) {
+     var c = ca[i];
+     while (c.charAt(0)==' ') c = c.substring(1);
+     if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+   }
+   return "";
+ }
+ function deleteCookie(name) {
+   document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+ };
+ function logout() { 
+   FB.api('/'+userId+'/permissions', 'delete',{ access_token : token } ,function(){ });
+   window.location= "login.html";
+ }
+ function getUrlParameter(sParam)
+ {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) 
+  {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam) 
     {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
+      return sParameterName[1];
     }
-	return null;
+  }
+  return null;
 }  
 
 function fbEnsureInit(callback) { // fuente: http://stackoverflow.com/questions/3548493/how-to-detect-when-facebooks-fb-init-is-complete
-        if(!window.fbApiInit) {
-            setTimeout(function() {fbEnsureInit(callback);}, 200);
-        } else {
-            if(callback) {
-                callback();
-            }
-        }
+  if(!window.fbApiInit) {
+    setTimeout(function() {fbEnsureInit(callback);}, 200);
+  } else {
+    if(callback) {
+      callback();
     }
-	
+  }
+}
+
 var isMobile = { //mover a commons fuente: http://www.abeautifulsite.net/detecting-mobile-devices-with-javascript/
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+  }
 };
 
 function timeConverter(UNIX_timestamp){
  var a = new Date(UNIX_timestamp*1000);
  var months = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
-     var year = a.getFullYear();
-     var month = months[a.getMonth()];
-     var date = a.getDate();
-     var hour = a.getHours();
-     var min = a.getMinutes();
-     var sec = a.getSeconds();
+ var year = a.getFullYear();
+ var month = months[a.getMonth()];
+ var date = a.getDate();
+ var hour = a.getHours();
+ var min = a.getMinutes();
+ var sec = a.getSeconds();
      var time = date + '-' + month + '-' + year; //+ ' ' + hour + ':' + min + ':' + sec ;
      return time;
- }
- 
- function getFormattedDate(UNIX_timestamp){
-	 var date = new Date(UNIX_timestamp*1000);
-	 var months = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
+   }
+   
+   function getFormattedDate(UNIX_timestamp){
+     var date = new Date(UNIX_timestamp*1000);
+     var months = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
      return date.getDate()+' de '+months[date.getMonth()] +' de '+date.getFullYear()  +' a las '+(date.getHours()<10?('0'+date.getHours()):date.getHours()) + ':' + ((date.getMinutes()<10)?('0'+date.getMinutes()):date.getMinutes());
- }
- 
+   }
+   
 function shareOnFb(itemId){ //fuente: http://www.walkswithme.net/facebook-share-button-with-custom-parameters
 	$('#cargandoModal').modal('show');
-			$.getJSON('/api/users/'+userId+'/items/'+itemId, function (data) {
-					console.log(data);
-					var product_name   = 	'¡He agregado un nuevo item en TruequeLibre!';
-					var description	   =	'Entra a mi perfil para ver mi nuevo item! TruequeLibre es una plataforma de intercambio directo de artículos libre de impuestos. La más utilizada en LatinoAmérica!';
-					var share_image	   =	data.img;
-					var share_url	   =	'https://hercules-tacs.appspot.com/profile.html?id='+userId+'&item='+itemId;	
-					var share_capt     =    data.name;
-					$('#cargandoModal').modal('hide');
-					FB.ui({
-						method: 'feed',
-						name: product_name,
-						link: share_url,
-						picture: share_image,
-						caption: share_capt,
-						description: description
+ $.getJSON('/api/users/'+userId+'/items/'+itemId, function (data) {
+   console.log(data);
+   var product_name   = 	'¡He agregado un nuevo item en TruequeLibre!';
+   var description	   =	'Entra a mi perfil para ver mi nuevo item! TruequeLibre es una plataforma de intercambio directo de artículos libre de impuestos. La más utilizada en LatinoAmérica!';
+   var share_image	   =	data.img;
+   var share_url	   =	'https://hercules-tacs.appspot.com/profile.html?id='+userId+'&item='+itemId;	
+   var share_capt     =    data.name;
+   $('#cargandoModal').modal('hide');
+   FB.ui({
+    method: 'feed',
+    name: product_name,
+    link: share_url,
+    picture: share_image,
+    caption: share_capt,
+    description: description
 
-					}, function(response){});
-					
-					
-			});
+  }, function(response){});
+   
+   
+ });
 }
 
 function shareTradeOnFb(tradeId, tradeSolicitado){ //fuente: http://www.walkswithme.net/facebook-share-button-with-custom-parameters
 	$('#cargandoModal').modal('show');
-			$.getJSON('/api/pendingTrades/'+tradeId, function (data) {
-					console.log(data);
-					var otherPerson;
-					var myItem;
-					var otherItem;
-					if(tradeSolicitado){
-						otherPerson= data.trade.wantedItem.ownerName;
-						myItem=data.trade.offeredItem.name;
-						otherItem=data.trade.wantedItem.name;
-					}else{
-						otherPerson= data.trade.offeredItem.ownerName;
-						myItem=data.trade.wantedItem.name;
-						otherItem=data.trade.offeredItem.name;
-					}
-					var product_name   = 	'¡He intercambiado un nuevo item en TruequeLibre!';
-					var description	   =	'He intercambiado a '+otherPerson+' mi "'+myItem+'" por su "'+otherItem+'"!';
-					var share_image	   =	data.trade.wantedItem.img;
-					var share_url	   =	'https://hercules-tacs.appspot.com/profile.html?id='+userId;	
-					var share_capt     =    'TruequeLibre, la plataforma de intercambios más utilizada!';
-					$('#cargandoModal').modal('hide');
-					FB.ui({
-						method: 'feed',
-						name: product_name,
-						link: share_url,
-						picture: share_image,
-						caption: share_capt,
-						description: description
+ $.getJSON('/api/pendingTrades/'+tradeId, function (data) {
+   console.log(data);
+   var otherPerson;
+   var myItem;
+   var otherItem;
+   if(tradeSolicitado){
+    otherPerson= data.trade.wantedItem.ownerName;
+    myItem=data.trade.offeredItem.name;
+    otherItem=data.trade.wantedItem.name;
+  }else{
+    otherPerson= data.trade.offeredItem.ownerName;
+    myItem=data.trade.wantedItem.name;
+    otherItem=data.trade.offeredItem.name;
+  }
+  var product_name   = 	'¡He intercambiado un nuevo item en TruequeLibre!';
+  var description	   =	'He intercambiado a '+otherPerson+' mi "'+myItem+'" por su "'+otherItem+'"!';
+  var share_image	   =	data.trade.wantedItem.img;
+  var share_url	   =	'https://hercules-tacs.appspot.com/profile.html?id='+userId;	
+  var share_capt     =    'TruequeLibre, la plataforma de intercambios más utilizada!';
+  $('#cargandoModal').modal('hide');
+  FB.ui({
+    method: 'feed',
+    name: product_name,
+    link: share_url,
+    picture: share_image,
+    caption: share_capt,
+    description: description
 
-					}, function(response){});
-					
-					
-			});
+  }, function(response){});
+  
+  
+});
 }
 
 function loadNotifications(){
 	$.getJSON('/api/pendingTrades', function (data) {
-				console.log(data);
-				$('#header_inbox_bar').find( "span.badge" ).html(data.receivedTradeRequests.length);
-				$('#header_inbox_bar').find( "ul" ).empty();
-				$('#header_inbox_bar').find( "ul" ).append('<div class="notify-arrow notify-arrow-red"></div> \
-                            <li> \
-                                <p class="red">Tienes '+data.receivedTradeRequests.length+' pedidos de intercambio</p> \
-                            </li>');
-				data.receivedTradeRequests.forEach(function(tradeReq){
-					$('#header_inbox_bar').find( "ul" ).append('<li> \
-					<a href="tradeRequests.html#recibidas"> \
-					<span class="photo"><img  src="'+tradeReq.offeredItem.img.slice(0,-5)+'I.jpg"></span> \
-					<span class="photo"><img  src="'+tradeReq.wantedItem.img.slice(0,-5)+'I.jpg" style="float: right;"></span> \
-					<span class="subject"><span class="from">'+tradeReq.offeredItem.ownerName+'</span></span> \
-					<span class="message"></i> '+tradeReq.offeredItem.name+'</span> \
-					<span class="message"><i class="icon-exchange" style="margin-left: 48%;"></i></span> \
-					<span class="message"></i> '+tradeReq.wantedItem.name+'</span> \
-					</a></li><li>');
-				});
-				$('#header_inbox_bar').find( "ul" ).append('<li> \
-                                <a href="tradeRequests.html#recibidas">Ver Todas</a> \
-                            </li>');
-        })
+    console.log(data);
+    $('#header_inbox_bar').find( "span.badge" ).html(data.receivedTradeRequests.length);
+    $('#header_inbox_bar').find( "ul" ).empty();
+    $('#header_inbox_bar').find( "ul" ).append('<div class="notify-arrow notify-arrow-red"></div> \
+      <li> \
+      <p class="red">Tienes '+data.receivedTradeRequests.length+' pedidos de intercambio</p> \
+      </li>');
+    data.receivedTradeRequests.forEach(function(tradeReq){
+     $('#header_inbox_bar').find( "ul" ).append('<li> \
+       <a href="tradeRequests.html#recibidas"> \
+       <span class="photo"><img  src="'+tradeReq.offeredItem.img.slice(0,-5)+'I.jpg"></span> \
+       <span class="photo"><img  src="'+tradeReq.wantedItem.img.slice(0,-5)+'I.jpg" style="float: right;"></span> \
+       <span class="subject"><span class="from">'+tradeReq.offeredItem.ownerName+'</span></span> \
+       <span class="message"></i> '+tradeReq.offeredItem.name+'</span> \
+       <span class="message"><i class="icon-exchange" style="margin-left: 48%;"></i></span> \
+       <span class="message"></i> '+tradeReq.wantedItem.name+'</span> \
+       </a></li><li>');
+   });
+    $('#header_inbox_bar').find( "ul" ).append('<li> \
+      <a href="tradeRequests.html#recibidas">Ver Todas</a> \
+      </li>');
+  })
 }	
-	
