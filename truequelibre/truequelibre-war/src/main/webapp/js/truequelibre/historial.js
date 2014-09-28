6d6a41329db848cf5f6fc608d64910410dcc0224
fede1608 <fedecasares1993@yahoo.com.ar>
2014-09-28 20:43:24 -0300

    Refactor AngularJS: historial.html

--- truequelibre/truequelibre-war/src/main/webapp/js/truequelibre/historial.js
+++ truequelibre/truequelibre-war/src/main/webapp/js/truequelibre/historial.js
@@ -18,21 +18,56 @@ function historialController($scope) {
 		var date = new Date(UNIX_timestamp*1000);
 		var months = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
 		return date.getDate()+' de '+months[date.getMonth()] +' de '+date.getFullYear()  +' a las '+(date.getHours()<10?('0'+date.getHours()):date.getHours()) + ':' + ((date.getMinutes()<10)?('0'+date.getMinutes()):date.getMinutes());
-		
+
 	};
-	$.getJSON('/api/pendingTrades', { state : 1 } , function (data) {
-		console.log(data);
-		$scope.receivedTrades=data.receivedTradeRequests;
-		$scope.sentTrades=data.sentTradeRequests;    
-		$scope.$apply();
-	})
-	$.getJSON('/api/pendingTrades', { state : 2 } , function (data) {
-		console.log(data);
-		$scope.rejectedReceivedTrades=data.receivedTradeRequests;
-		$scope.rejectedSentTrades=data.sentTradeRequests;
-		$scope.$apply();
-		$('#cargandoModal').modal('hide');
-	})
+	$scope.shareOnFB= function (tradeId, tradeSolicitado){ //fuente: http://www.walkswithme.net/facebook-share-button-with-custom-parameters
+		$('#cargandoModal').modal('show');
+		$.getJSON('/api/pendingTrades/'+tradeId, function (data) {
+			console.log(data);
+			var otherPerson;
+			var myItem;
+			var otherItem;
+			if(tradeSolicitado){
+				otherPerson= data.trade.wantedItem.ownerName;
+				myItem=data.trade.offeredItem.name;
+				otherItem=data.trade.wantedItem.name;
+			}else{
+				otherPerson= data.trade.offeredItem.ownerName;
+				myItem=data.trade.wantedItem.name;
+				otherItem=data.trade.offeredItem.name;
+			}
+			var product_name   = 	'¡He intercambiado un nuevo item en TruequeLibre!';
+			var description	   =	'He intercambiado a '+otherPerson+' mi "'+myItem+'" por su "'+otherItem+'"!';
+			var share_image	   =	data.trade.wantedItem.img;
+			var share_url	   =	'https://hercules-tacs.appspot.com/profile.html?id='+userId;	
+			var share_capt     =    'TruequeLibre, la plataforma de intercambios más utilizada!';
+			$('#cargandoModal').modal('hide');
+			FB.ui({
+				method: 'feed',
+				name: product_name,
+				link: share_url,
+				picture: share_image,
+				caption: share_capt,
+				description: description
+
+			}, function(response){});
+
+
+		});
+}
+$.getJSON('/api/pendingTrades', { state : 1 } , function (data) {
+	console.log(data);
+	$scope.receivedTrades=data.receivedTradeRequests;
+	$scope.sentTrades=data.sentTradeRequests;    
+	$scope.$apply();
+})
+$.getJSON('/api/pendingTrades', { state : 2 } , function (data) {
+	console.log(data);
+	$scope.rejectedReceivedTrades=data.receivedTradeRequests;
+	$scope.rejectedSentTrades=data.sentTradeRequests;
+	$scope.$apply();
+	$('#cargandoModal').modal('hide');
+})
 
 }
 
