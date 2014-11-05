082e4a053dae402177abed6d8e5c69a1ce939abe
Jonathan Coralo <jonathan.corallo92@gmail.com>
2014-11-05 15:00:10 -0300

    Tests innecesarios

--- truequelibre/truequelibre-war/src/main/test/com/hercules/truequelibre/tests/TestGAE.java
+++ truequelibre/truequelibre-war/src/main/test/com/hercules/truequelibre/tests/TestGAE.java
@@ -9,9 +9,6 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import com.google.appengine.api.memcache.MemcacheService;
-import com.google.appengine.api.memcache.MemcacheServiceFactory;
-import com.google.appengine.tools.development.testing.LocalMemcacheServiceTestConfig;
 import com.googlecode.objectify.ObjectifyService;
 import com.hercules.truequelibre.domain.InexistentObjectException;
 import com.hercules.truequelibre.domain.ItemNotExistsException;
@@ -26,9 +23,6 @@ public class TestGAE {
 	private final LocalServiceTestHelper datastoreHelper =
 			new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
 	
-	private final LocalServiceTestHelper memcacheHelper =
-		    new LocalServiceTestHelper(new LocalMemcacheServiceTestConfig());
-	
 	private String idMLItem = "MLA528604866";	//id de una video camara samsung TODO reemplazar por una busqueda y obtener el ID del primer resultado
 	
 	static{
@@ -39,7 +33,6 @@ public class TestGAE {
 	@Before
 	public void setUp() {
 		datastoreHelper.setUp();
-		memcacheHelper.setUp();
 	}
 
 	@After
@@ -58,16 +51,6 @@ public class TestGAE {
 		ds.put(new Entity("prueba"));
 		assertEquals(2, ds.prepare(new Query("prueba")).countEntities(withLimit(10)));
 	}
-	
-	/**
-	 * Test de memcache (cache de memoria)
-	 */
-	private void testMemcache() {
-	    MemcacheService ms = MemcacheServiceFactory.getMemcacheService();
-	    assertFalse(ms.contains("clavePrueba"));
-	    ms.put("clavePrueba", "valorDePrueba");
-	    assertTrue(ms.contains("clavePrueba"));
-	}
 
 	@Test
 	public void testInsertDataStore1() {
@@ -77,16 +60,6 @@ public class TestGAE {
 	@Test
 	public void testInsertDataStore2() {
 		testDatastore();
-	}
-	
-	@Test
-	public void testInsertmemcache1() {
-		testMemcache();
-	}
-
-	@Test
-	public void testInsertmemcache2() {
-		testMemcache();
 	}
 	
 	public long insertarItem() {
